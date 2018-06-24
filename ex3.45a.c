6386d087c0cdd1d71f7a94623870162118c25b38
CJ <chijioke.ahuchogu@yahoo.com>
2018-06-24 06:41:31 -0500

    Create ex3.45a.c

---
+++ ex3.45a.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* Exercise 3.45a
+ 3 [C : How to Program, 7th Edition by Paul Deitel, Harvey Deitel]
+ Solution provided by Chijioke Ahuchogu     */
+
+// A program that reads a non negative integer and finds its factorial
+
+
+
+
+int main()
+{
+    int number , factorial = 1 ;
+    
+    printf("Input any numper to find n!: ");
+    scanf("%d",&number);
+    
+    if ( number < 0 ) {
+        printf("Invalid Input, pls enter any non negative integer to find n! : ");
+        scanf("%d",&number);
+    }
+    
+    if ( number == 0 ) {
+        factorial = 1;
+    }
+    
+    while ( number != 1) {
+        
+        factorial =  factorial  * number ;
+        number--;
+    }
+    
+    printf( "n! is : %d \n" , factorial );
+
+}
