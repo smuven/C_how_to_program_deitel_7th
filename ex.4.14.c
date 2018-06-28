e943612004aa03bcb74627a22150935a657428f0
CJ <chijioke.ahuchogu@yahoo.com>
2018-06-28 08:34:52 -0500

    Create ex.4.14.c

---
+++ ex.4.14.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+
+/* Exercise 4.14
+[C : How to Program, 7th Edition by Paul Deitel, Harvey Deitel]
+ Solution provided by Chijioke Ahuchogu  */
+
+// factorials problem
+
+
+
+
+
+int main()
+{
+    int i , f ;
+    int n = 5;
+    
+    for (i = 1; i <= 10; i++){
+        n = i;
+        f = 1;
+        while (n != 0) {
+            f = f * n;
+            n--;
+        }// exits while loop
+        printf("%d\t\t%d\n", i, f);
+    }// exits for loop
+    
+     printf("\n\n");
+    
+    return 0;
+}
+
